20c74fa816fe0ac34ac91270eb295637beab9ee8
Florian Feka <florianfeka05@gmail.com>
2023-03-25 19:14:10 +0000

    fix delete on compile

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 FILENAME=$1
 
 pdflatex $1.tex && bibtex $1.aux && pdflatex $1.tex && pdflatex $1.tex
-
-rm $FILENAME.aux && $FILENAME.bbl && $FILENAME.blg && $FILENAME.lof && $FILENAME.lof && $FILENAME.log && $FILENAME.lol && $FILENAME.lot && $FILENAME.toc
+sleep 2
+rm $FILENAME.aux && rm $FILENAME.bbl && rm $FILENAME.blg && rm $FILENAME.lof && rm $FILENAME.lol && rm $FILENAME.log && rm $FILENAME.lot && rm $FILENAME.toc
