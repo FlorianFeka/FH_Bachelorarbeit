f9ee3769fc648bfca9d577ee99e31bf1dad89eb8
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2023-04-25 21:44:15 +0200

    first version of theorie

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-FILENAME=${1:--Bachelor}
+FILENAME=$1
 
-pdflatex $1.tex && bibtex $1.aux && pdflatex $1.tex && pdflatex $1.tex
+rm "$FILENAME.pdf"; rm "$FILENAME.aux"; rm "$FILENAME.bbl"; rm "$FILENAME.blg"; rm "$FILENAME.lof"; rm "$FILENAME.lol"; rm "$FILENAME.log"; rm "$FILENAME.lot"; rm "$FILENAME.toc"
 sleep 2
-rm $FILENAME.aux && rm $FILENAME.bbl && rm $FILENAME.blg && rm $FILENAME.lof && rm $FILENAME.lol && rm $FILENAME.log && rm $FILENAME.lot && rm $FILENAME.toc
+pdflatex "$FILENAME.tex" && sleep 8 && bibtex "$FILENAME.aux" && sleep 8 && pdflatex "$FILENAME.tex" && sleep 8 && pdflatex "$FILENAME.tex"
+sleep 2
+rm "$FILENAME.aux"; rm "$FILENAME.bbl"; rm "$FILENAME.blg"; rm "$FILENAME.lof"; rm "$FILENAME.log"; rm "$FILENAME.toc"
